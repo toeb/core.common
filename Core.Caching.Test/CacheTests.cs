5a371fe48f70a8fca1053bf86824695392ad8f57
Tobias Becker <becker.tobi@gmai.com>
2015-04-27 11:53:10 +0200

    added cache project

---
+++ Core.Caching.Test/CacheTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Core.Caching.Test
+{
+  [TestClass]
+  public class CacheTests
+  {
+    [TestMethod]
+    public void TestMethod1()
+    {
+
+
+
+    }
+  }
+}
---
+++ Core.Caching/ICache.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Caching
+{
+  public static class ICacheExtensions
+  {
+
+  }
+  public class ICache
+  {
+
+  }
+}
